79ea93314f168bb3356cb3d3bc2557b80aeb6ac1
PeterKitonga <pkitonga.pk@gmail.com>
2019-04-13 01:10:40 +0300

    Added a seeder class for dummy users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $guzzle = new \GuzzleHttp\Client();
+
+        $response = $guzzle->get('https://randomuser.me/api/?page=1&results=100&nat=gb&seed=9a7cbf079c0ad2ef');
+
+        $users = json_decode((string) $response->getBody(), true);
+
+        foreach ($users['results'] as $user) {
+            $subscriber = \App\User::query()->create([
+                'name' => $user['name']['first'].' '.$user['name']['last'],
+                'email' => $user['email'],
+                'password' => \Illuminate\Support\Facades\Hash::make($user['login']['password']),
+                'profile_image' => $user['picture']['large'],
+                'is_active' => 1,
+                'email_verified_at' => Carbon\Carbon::now()->toDateTimeString()
+            ]);
+
+            DB::table('role_user')->insert([
+                [
+                    'role_id' => 2,
+                    'user_id' => $subscriber->id
+                ]
+            ]);
+        }
+    }
+}
