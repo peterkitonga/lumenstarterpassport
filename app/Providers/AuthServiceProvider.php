e4d32c6fc37274cb87303396ad6e0c41c6585fe3
PeterKitonga <pkitonga.pk@gmail.com>
2019-04-12 14:20:10 +0300

    Minor update to the auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -28,16 +27,5 @@ class AuthServiceProvider extends ServiceProvider
     {
         \Dusterio\LumenPassport\LumenPassport::routes($this->app, ['prefix' => 'api/v1/oauth']);
         \Dusterio\LumenPassport\LumenPassport::tokensExpireIn(Carbon::now()->addMonth(), env('PASSWORD_GRANT_CLIENT_ID'));
-
-        // Here you may define how you wish users to be authenticated for your Lumen
-        // application. The callback which receives the incoming request instance
-        // should return either a User instance or null. You're free to obtain
-        // the User instance via an API token or any other method necessary.
-
-        $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
-            }
-        });
     }
 }
