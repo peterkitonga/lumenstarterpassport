18359a93663feb3db2d61cd4ed74cf6f1f6d9ec7
PeterKitonga <pkitonga.pk@gmail.com>
2019-04-14 01:05:08 +0300

    Added gates to the auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -27,5 +29,27 @@ class AuthServiceProvider extends ServiceProvider
     {
         \Dusterio\LumenPassport\LumenPassport::routes($this->app, ['prefix' => 'api/v1/oauth']);
         \Dusterio\LumenPassport\LumenPassport::tokensExpireIn(Carbon::now()->addMonth(), env('PASSWORD_GRANT_CLIENT_ID'));
+
+        $this->registerPolicies();
+    }
+
+    /**
+     * Checks for the user's access permissions
+     *
+     * @return void
+     */
+    public function registerPolicies()
+    {
+        if (Schema::connection(env('DB_CONNECTION'))->hasTable('roles'))
+        {
+            $roles = \App\Role::query()->pluck('slug');
+
+            foreach ($roles as $role)
+            {
+                Gate::define($role, function (User $user) use ($role) {
+                    return $user->inRole($role);
+                });
+            }
+        }
     }
 }
