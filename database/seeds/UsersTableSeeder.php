09ba901176008d6f1503758670e08daa48854327
PeterKitonga <pkitonga.pk@gmail.com>
2019-05-04 15:27:09 +0300

    Modified the users table seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,26 +14,30 @@ class UsersTableSeeder extends Seeder
     {
         $guzzle = new \GuzzleHttp\Client();
 
-        $response = $guzzle->get('https://randomuser.me/api/?page=1&results=100&nat=gb&seed=9a7cbf079c0ad2ef');
+        $response = $guzzle->get('https://randomuser.me/api/?page=1&results=200&nat=gb&seed=9a7cbf079c0ad2ef');
 
         $users = json_decode((string) $response->getBody(), true);
 
         foreach ($users['results'] as $user) {
-            $subscriber = \App\User::query()->create([
-                'name' => $user['name']['first'].' '.$user['name']['last'],
-                'email' => $user['email'],
-                'password' => \Illuminate\Support\Facades\Hash::make($user['login']['password']),
-                'profile_image' => $user['picture']['large'],
-                'is_active' => 1,
-                'email_verified_at' => Carbon\Carbon::now()->toDateTimeString()
-            ]);
+            $check = \App\User::query()->where('email', 'LIKE', '%'.$user['email'].'%')->first();
 
-            DB::table('role_user')->insert([
-                [
-                    'role_id' => 2,
-                    'user_id' => $subscriber->id
-                ]
-            ]);
+            if (!$check) {
+                $subscriber = \App\User::query()->create([
+                    'name' => $user['name']['first'].' '.$user['name']['last'],
+                    'email' => $user['email'],
+                    'password' => \Illuminate\Support\Facades\Hash::make($user['login']['password']),
+                    'profile_image' => $user['picture']['large'],
+                    'is_active' => 1,
+                    'email_verified_at' => Carbon\Carbon::now()->toDateTimeString()
+                ]);
+
+                DB::table('role_user')->insert([
+                    [
+                        'role_id' => 2,
+                        'user_id' => $subscriber->id
+                    ]
+                ]);
+            }
         }
     }
 }
